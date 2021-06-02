b46d59316be76e0fe9208256de7c2acebe048ef0
pbhuniar <82756479+pbhuniar@users.noreply.github.com>
2021-06-02 18:42:29 +0530

    Update Page.jsx

--- src/Component/Page.jsx
+++ src/Component/Page.jsx
@@ -1,31 +1,48 @@
-import React,{useState} from 'react'
-import './Page.css'
+import React, { useState } from 'react'
 
 const Page = () => {
-    const [color,setcolor] =useState("red") 
-    const [hide,display] = useState("hidden")
-    var sheight = window.innerHeight/2;
-    var swidth = window.innerWidth/2;
+    const [color, setcolor] = useState("red")
+    const [hide, display] = useState("hidden")
+    const [count, countup] = useState(0)
 
 
-    const chngcolor =()=>{
 
-        setcolor("green")
-        display("")
+    const chngcolor = () => {
+        countup(count + 1)
+
+        if (color === "red") {
+            setcolor("green")
+            display("")
+        }
+        else {
+            setcolor("red")
+        }
+
     }
-    
+
+    const block1 = {
+        height: "50%",
+        width: "50%",
+        background: color,
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%,-50%)"
+    }
+
     return (
         <div>
-            <div style={{height:sheight,width:swidth,background:color,position:"absolute",top:"50%",left:"50%",transform:"translate(-50%,-50%)"}}>
+            <div style={block1}>
             </div>
             <br />
             <br />
-<div style={{position:"absolute",top:"90%",left:"50%",transform:"translate(-50%,-50%)"}}>
-<button style={{height:"3rem",width:"7rem",background:"red",color:"white",border:"none"}} onClick={chngcolor} >Press</button>
-<h4 style={{visibility:hide}}>Button Clicked</h4>
-</div>
-           
-           
+            <div style={{ position: "absolute", top: "90%", left: "50%", transform: "translate(-50%,-50%)" }}>
+                <button style={{ height: "3rem", width: "100%", background: "red", color: "white", border: "none" }} onClick={chngcolor} >Press</button>
+                <p style={{ visibility: hide, marginBottom: "0px" }}>Button Clicked</p>
+                <p style={{ visibility: hide, textAlign: "center" }}>{count} times</p>
+            </div>
+
+
         </div>
     )
 }
